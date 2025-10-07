1d0cac6b244d79c8e19d761c7da24853671a0bad
Long Đặng Hữu <danghuulong394@gmail.com>
2025-10-07 16:03:57 +0700

    feat(home): implement header for home page

---
+++ src/pages/Home/header.jsx
@@ -0,0 +1,108 @@
+function Header() {
+  return (
+    <>
+      <div style={{
+        height: 100,
+        backgroundColor: 'rgba(255,255,255,1)',
+        padding: '0px 100px 0px 54px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+        {/* Logo Section */}
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 5,
+        }}>
+          <img src="./src/pages/Home/images/Meubel House_Logos-05.png" style={{
+            width: 50,
+            height: 32,
+          }}/>
+          <p style={{
+            fontFamily: '"Montserrat", sans-serif',
+            fontWeight: 700,
+            fontSize: 34,
+            textTransform: 'uppercase',
+            color: '#000000ff',                        
+          }}>Furniro</p>
+        </div>
+
+        {/* Navigation Section */}
+        <div style={{
+          width: 430,
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginLeft: 266,
+          marginRight: 'auto',
+        }}>
+          <p style={{
+            fontFamily: '"Poppins", sans-serif',
+            fontWeight: 500,
+            fontSize: 16,
+            color: '#000000ff',                        
+          }}>
+            Home
+          </p>
+          <p style={{
+            fontFamily: '"Poppins", sans-serif',
+            fontWeight: 500,
+            fontSize: 16,
+            color: '#000000ff',
+          }}>
+            Shop
+          </p>
+          <p style={{
+            fontFamily: '"Poppins", sans-serif',
+            fontWeight: 500,
+            fontSize: 16,
+            color: '#000000ff',
+          }}>
+            About
+          </p>
+          <p style={{
+            fontFamily: '"Poppins", sans-serif',
+            fontWeight: 500,
+            fontSize: 16,
+            color: '#000000ff',
+          }}>
+            Contact
+          </p>
+        </div>
+
+        {/* Icons Section */}
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 45,
+        }}>
+          <img src="./src/pages/Home/images/mdi_account-alert-outline.png" style={{
+            width: 28,
+            height: 28,
+          }} />
+          <img src="./src/pages/Home/images/akar-icons_search.png" style={{
+            width: 28,
+            height: 28,
+          }} />
+          <img src="./src/pages/Home/images/akar-icons_heart.png" style={{
+            width: 28,
+            height: 28,
+          }} />
+          <img src="./src/pages/Home/images/ant-design_shopping-cart-outlined.png" style={{
+            width: 28,
+            height: 28,
+          }} />
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
