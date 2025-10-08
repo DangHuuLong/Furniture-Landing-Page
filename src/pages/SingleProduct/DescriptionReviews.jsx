93939b1f9be3650b901c008c73130a73916dd952
Long Đặng Hữu <danghuulong394@gmail.com>
2025-10-08 21:54:23 +0700

    feat(single product): implement description reviews

---
+++ src/pages/SingleProduct/DescriptionReviews.jsx
@@ -0,0 +1,94 @@
+function DescriptionReviews(){
+  return (
+    <div style={{
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}>
+      <div style={{
+        width: '100%',
+        height: '1px',
+        backgroundColor: 'rgba(217,217,217,1)',
+        marginBottom: '48px',
+      }}></div>
+      <div style={{
+        display: 'flex',
+        gap: '52px',
+        marginBottom: '37px'
+      }}>
+        <p style={{
+          fontFamily: '"Poppins", sans-serif',
+          fontWeight: 500,
+          fontSize: '24px',
+          color: 'rgba(0,0,0,1)'
+        }}>Description</p>
+        <p style={{
+          fontFamily: '"Poppins", sans-serif',
+          fontWeight: 500,
+          fontSize: '24px',
+          color: 'rgba(159,159,159,1)'
+        }}>Additional Information</p>
+        <p style={{
+          fontFamily: '"Poppins", sans-serif',
+          fontWeight: 500,
+          fontSize: '24px',
+          color: 'rgba(159,159,159,1)'
+        }}>Reviews [5]</p>
+      </div>
+      <p style={{
+        width: '1026px',
+        flexWrap: 'wrap',
+        fontFamily: '"Poppins", sans-serif',
+        fontWeight: 400,
+        fontSize: '16px',
+        color: 'rgba(159,159,159,1',
+        marginBottom: '30px',
+      }}>Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.</p>
+      <p style={{
+        width: '1026px',
+        flexWrap: 'wrap',
+        fontFamily: '"Poppins", sans-serif',
+        fontWeight: 400,
+        fontSize: '16px',
+        color: 'rgba(159,159,159,1)',
+        marginBottom: '36px',
+      }}>
+        Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.
+      </p>
+      <div style={{
+        display: 'flex',
+        gap: '30px',
+        marginBottom: '65px'
+      }}>
+        <div style={{
+          width: '605',
+          height: '348',
+          borderRadius: '10px',
+          backgroundColor: 'rgba(249,241,231,1)'
+        }}>
+          <img src='/src/pages/SingleProduct/images/Cloud sofa three seater + ottoman_2 1.png' style={{
+            width: '100%'
+          }}/>
+        </div>
+        <div style={{
+          width: '605',
+          height: '348',
+          borderRadius: '10px',
+          backgroundColor: 'rgba(249,241,231,1)'
+        }}>
+          <img src='/src/pages/SingleProduct/images/Cloud sofa three seater + ottoman_1 1.png' style={{
+            width: '100%'
+          }} />
+        </div>
+      </div>
+      <div style={{
+        width: '100%',
+        height: '1px',
+        backgroundColor: 'rgba(217,217,217,1)',
+        marginBottom: '48px',
+      }}></div>
+    </div>
+  )
+}
+export default DescriptionReviews
--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,10 +1,12 @@
 import TabBar from "./TabBar"
 import DetailsProduct from "./DetailsProduct"
+import DescriptionReviews from "./DescriptionReviews"
 function SingleProduct() {
   return (
     <>
       <TabBar />
       <DetailsProduct />
+      <DescriptionReviews />
     </>
   )
 }
