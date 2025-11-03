349c2e13db116a111a15f1eb0aa2d3bb2a02e986
Long Đặng Hữu <danghuulong394@gmail.com>
2025-11-03 07:39:55 +0700

    feat(knowledge-base): implement KnowledgeBasePage layout with search input

--- frontend/src/modules/admin/pages/knowledge_base/knowledge_base_page.jsx
+++ frontend/src/modules/admin/pages/knowledge_base/knowledge_base_page.jsx
@@ -1,3 +1,4 @@
+import {Search} from 'lucide-react'
 export default function KnowledgeBasePage(){
   return (
     <div style={{
@@ -7,7 +8,40 @@ export default function KnowledgeBasePage(){
       backgroundColor: '#F5F6FA',
       padding: '30px 40px 40px 40px',
     }}>
-      
+      <p style={{
+        fontFamily: '"Poppins", sans-serif',
+        fontWeight: 700,
+        fontSize: 24,
+        color: '#131523',
+      }}>Knowledge Base</p>
+
+      {/**Search */}
+      <div style={{
+        marginTop: 54,
+        position: 'relative'
+      }}>
+        <input type="text" 
+          placeholder="Search..."
+        style={{
+          width: '100%',
+          height: 48,
+          borderRadius: 4,
+          border: '1px solid #D9E1EC',
+          outline: 'none',
+          paddingLeft: 48,
+          fontFamily: '"Poppins", sans-serif',
+          fontWeight: 400,
+          fontSize: 16,
+          color: '#A1A7C4',
+        }}/>
+        <div style={{
+          position: 'absolute',
+          top: '25%',
+          left: 15,
+        }}>
+          <Search size={24} color='#7E84A3'/>
+        </div>
+      </div>
     </div>
   )
 }
